1b8cbbfdd6c738fb91918e6be2eb945b04bc59b7
Robert Daub <rjd2066@gmail.com>
2020-04-09 19:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+function theBeatlesPlay(musicians = ['John Lennon'], instruments = ['Guitar']) {
+  consts arr = []
 
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr;
+}
