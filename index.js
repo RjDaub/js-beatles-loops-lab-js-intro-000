47520ba47d28bd53bb223975d4b661682c757e69
Robert Daub <rjd2066@gmail.com>
2020-04-09 19:31:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function theBeatlesPlay(musicians = 'John Lennon', intruments = 'Guitar') {
   const arr = []
-
-  for (let i = 0, 1 = musicians.length; i < 1; i++) {
+for (let i = 0; 1 = musicians.length; i < 1; i++) {
     arr.push(`${musicians[i]} plays ${intruments[i]}`)
   }
   return arr;
