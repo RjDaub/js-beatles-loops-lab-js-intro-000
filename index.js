06f9ea8614495e7831ad0602305f5068039008d5
Robert Daub <rjd2066@gmail.com>
2020-04-09 19:24:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function theBeatlesPlay(); {
+function theBeatlesPlay(musicians, intruments) {
+  const arr = []
 
-}
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
+    arr.push(`${musicians[i]} plays ${intruments[i]}`)
+  }
+  return arr;
+      
+    }
+
+
